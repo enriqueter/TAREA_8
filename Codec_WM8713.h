f8875dc26c84a4f4f77f45f27cd5405d418c1931
Enrique  Teran <31829881+enriqueter@users.noreply.github.com>
2020-11-13 01:06:16 -0600

    Initial Commit empty files

---
+++ Codec_WM8713.h
@@ -0,0 +1,17 @@
+/*
+ * Codec_WM8713.h
+ *
+ *  Created on: 13 nov. 2020
+ *      Author: etera
+ */
+
+#ifndef CODEC_WM8713_H_
+#define CODEC_WM8713_H_
+
+#include "MK66F18.h"
+#include "FreeRTOS.h"
+#include "stdint.h"
+#include "FreeRtos_I2C.h"
+
+
+#endif /* CODEC_WM8713_H_ */
---
+++ FreeRtos_I2C.h
@@ -0,0 +1,30 @@
+/*
+ * FreeRtos_I2C.h
+ *
+ *  Created on: 13 nov. 2020
+ *      Author: etera
+ */
+
+#ifndef FREERTOS_I2C_H_
+#define FREERTOS_I2C_H_
+
+#include <stdint.h>
+
+typedef enum {rtos_i2c_0, rtos_i2c_1, rtos_i2c_2} rtos_i2c_number_t;
+typedef enum {rtos_i2c_portA,rtos_i2c_portB,rtos_i2c_portC,rtos_i2c_portD,rtos_i2c_portE} rtos_i2c_port_t;
+typedef enum {rtos_i2c_sucess,rtos_i2c_fail} rtos_i2c_flag_t;
+typedef struct
+{
+	uint32_t  baudrate;				/**I2C Baud rate*/
+	rtos_i2c_number_t i2c_number;	/**I2C to use*/
+	rtos_i2c_port_t port;			/**Kinietis Port*/
+	uint8_t SCL_pin;				/**Pin of Serial Clock*/
+	uint8_t SDA_pin;				/**Pin of Serial Data*/
+	uint8_t pin_mux;				/**Pin Configuration*/
+}rtos_i2c_config_t;
+
+rtos_i2c_flag_t rtos_i2c_init(rtos_i2c_config_t config);
+rtos_i2c_flag_t rtos_i2c_transfer(rtos_i2c_number_t i2c_number, uint8_t * buffer, uint16_t length, uint16_t slave_addr, uint16_t subaddr, uint8_t subsize);
+rtos_i2c_flag_t rtos_i2c_receive(rtos_i2c_number_t i2c_number, uint8_t * buffer, uint16_t length, uint16_t slave_addr, uint16_t subaddr, uint8_t subsize);
+
+#endif /* FREERTOS_I2C_H_ */
